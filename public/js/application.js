2e5392c15ebbf5796d6bc18114438a495579294d
dave <dave@dave-VirtualBox.(none)>
2013-07-11 14:57:28 -0700

    Added state density view overlay, made info/legend appear/disappear when overlay is enabled/disbled

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,35 @@
-jQuery(function() {
+//
+//  leaflet.js interface code 
+//  Information on leaflet at http://leafletjs.com/examples/quick-start.html
+//
+
+
+
+// Sets colors for each population density
+function getDensityColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}
+
+// Style each state area
+function densityStyle(feature) {
+    return {
+        fillColor: getDensityColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+$(function() {
 	var userId = Math.random().toString(16).substring(2,15);
 	var socket = io.connect("/");
 	var map;
@@ -53,37 +84,145 @@ socket.on("load:coords", function(data) {
  		var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/{styleId}/256/{z}/{x}/{y}.png',
    			cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
  
-        // leaflet API key tiler
+       // Set up base maps - only one base map can be displayed at a time
        var base = L.tileLayer("http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png", { maxZoom: 18, detectRetina: true });
        var style1= L.tileLayer(cloudmadeUrl, {styleId:1, maxZoom: 18, detectRetina: true, attribution: cloudmadeAttribution});
        var style2= L.tileLayer(cloudmadeUrl, {styleId:997, maxZoom: 18, detectRetina: true, attribution: cloudmadeAttribution });
        var style3= L.tileLayer(cloudmadeUrl, {styleId:7, maxZoom: 18, detectRetina: true, attribution: cloudmadeAttribution });
        var style4= L.tileLayer(cloudmadeUrl, {styleId:998, maxZoom: 18, detectRetina: true, attribution: cloudmadeAttribution});
-       var roads= L.tileLayer(cloudmadeUrl, {styleId:46561, maxZoom: 18, detectRetina: true, attribution: cloudmadeAttribution});
+	   var gray= L.tileLayer(cloudmadeUrl, {styleId:22677, maxZoom: 18, detectRetina: true, attribution: cloudmadeAttribution});
+
 
        var baseMaps = {
        	"Default": base,
        	"Style 1": style1,
        	"Style 2": style2,
        	"Style 3": style3,
-       	"Style 4": style4
+       	"Style 4": style4,
+       	"Gray": gray
        }
+
+       // Set up overlays
+       var roads= L.tileLayer(cloudmadeUrl, {styleId:46561, maxZoom: 18, detectRetina: true, attribution: cloudmadeAttribution});
+       // Information on geoJson:  http://leafletjs.com/examples/geojson.html
+       var states= L.geoJson(statesData, {style: densityStyle, onEachFeature: onEachFeature});
 
        var overlayMaps = {
-       	"Freeways": roads
+       	"Freeways": roads,
+       	"State Density": states
        }
 
+        // load leaflet map with "base" set as the default map
 
-        // load leaflet map
         map = L.map("map", {layers: [base]});
         L.control.layers(baseMaps,overlayMaps).addTo(map);
+
+
+
+        // Add Controls, Info, and legend to state overlay
+        // overlayadd and overlayremove events are used to control 
+        // visibility of info and legend
+
+        var stateInfo = L.control();
+
+        // When an overlay is added (clicked on) add the state info and legend
+        map.on('overlayadd', function(e) {
+    
+        	if (e.name == 'State Density') {
+        		//alert("States overlay added");
+        		stateInfo.addTo(map);
+        		stateLegend.addTo(map);
+        	}
+        });
+
+        // When an overlay is removed from view, remove the info and legend
+        map.on('overlayremove', function(e) {
+    
+        	if (e.name == 'State Density') {
+        		//alert("States overlay added");
+        		stateInfo.removeFrom(map);
+        		stateLegend.removeFrom(map);
+        	}
+        });
+
+		// This listener is attached only to the state layer.  So no other layers
+		// will have these controls
+		function onEachFeature(feature, layer) {
+		    layer.on({
+		        mouseover: highlightFeature,
+		        mouseout: resetHighlight,
+		        click: zoomToFeature
+		    });
+		}
+
+        function highlightFeature(e) {
+		    var layer = e.target;
+
+		    layer.setStyle({
+		        weight: 5,
+		        color: '#666',
+		        dashArray: '',
+		        fillOpacity: 0.7
+		    });
+
+		    if (!L.Browser.ie && !L.Browser.opera) {
+		        layer.bringToFront();
+		    }
+		    stateInfo.update(layer.feature.properties);
+		}
+
+		function resetHighlight(e) {
+		    states.resetStyle(e.target);
+		    stateInfo.update();
+		}
+
+		function zoomToFeature(e) {
+		    map.fitBounds(e.target.getBounds());
+		}
+
+		
+		// Add info panel  (do we end up creating multiple dom elements
+		//  if there are multiple add/removes?  Need to investigate)	
+		stateInfo.onAdd = function (map) {
+		    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+		    this.update();
+		    return this._div;
+		};
+
+		// method that we will use to update the control based on feature properties passed
+		stateInfo.update = function (props) {
+			    this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+			        '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+			        : 'Hover over a state');
+		};
+
+		var stateLegend = L.control({position: 'bottomright'});
+
+		stateLegend.onAdd = function (map) {
+
+		    var div = L.DomUtil.create('div', 'info legend'),
+		        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+		        labels = [];
+
+		    // loop through our density intervals and generate a label with a colored square for each interval
+		    for (var i = 0; i < grades.length; i++) {
+		        div.innerHTML +=
+		            '<i style="background:' + getDensityColor(grades[i] + 1) + '"></i> ' +
+		            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+		    }
+
+		    return div;
+		};
+
+		
+
          
         // set map bounds
         map.fitWorld();
         userMarker.addTo(map);
         userMarker.bindPopup("<p>You are here.</p>").openPopup();
  
-        // send coords on when user is active
+        // Send coordinates back to server
         doc.on("mousemove", function() {
             active = true; 
  
